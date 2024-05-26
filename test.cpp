933d56092b6fac2c4ace3393dc302652bcd6b3fe
ShuoCseu <cuishuo46@gmail.com>
2024-05-26 21:25:39 +0800

    add hello world

--- test.cpp
+++ test.cpp
@@ -1,33 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#include <iostream>
 
-// 线程函数
-void* thread_func(void* arg) {
-    int thread_num = *((int*)arg);
-    printf("I am thread %d\n", thread_num);
-    return NULL;
-}
-
-int main() {
-    pthread_t threads[3];
-    int thread_nums[3] = {1, 2, 3};
-
-    // 创建线程
-    for (int i = 0; i < 3; i++) {
-        if (pthread_create(&threads[i], NULL, thread_func, &thread_nums[i]) != 0) {
-            fprintf(stderr, "Error creating thread %d\n", i);
-            return 1;
-        }
-    }
-
-    // 等待线程完成
-    for (int i = 0; i < 3; i++) {
-        if (pthread_join(threads[i], NULL) != 0) {
-            fprintf(stderr, "Error joining thread %d\n", i);
-            return 2;
-        }
-    }
-
-    return 0;
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
 }
